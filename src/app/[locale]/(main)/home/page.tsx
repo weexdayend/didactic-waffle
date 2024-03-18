ed868dd0c41122981e75f4511c31be080c0f309d
Irvan Gerhana <weexdayend@gmail.com>
2024-03-18 21:48:17 +0700

    card realisasi

--- src/app/[locale]/(main)/home/page.tsx
+++ src/app/[locale]/(main)/home/page.tsx
@@ -16,21 +16,21 @@ const Page = () => {
           <h1 className='font-bold text-4xl'>10,000 ton</h1>
           <h6 className='text-sm dark:text-white/60'>{cs('description')}</h6>
         </div>
-        <div className='grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 w-full bg-blue-500 rounded-3xl shadow-lg shadow-black/5 px-4 py-6'>
+        <div className='grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 w-full bg-blue-500 rounded-3xl shadow-lg shadow-black/5 gap-6 px-6 py-6'>
           <div>
             <h4 className='text-base text-white'>{ca('realization')}</h4>
             <h1 className='text-3xl text-white font-bold'>8,000 ton</h1>
-            <div className='w-fit bg-white px-4 py-1 rounded-full mt-4'>
+            <div className='w-full bg-white px-4 py-1 rounded-full mt-4 drop-shadow-md'>
               <h6 className='text-sm text-zinc-800/70'><span className='text-green-500 font-bold'>+12.3%</span> {ca('notes')}</h6>
             </div>
           </div>
 
-          <div className='sm:block md:block lg:hidden xl:hidden h-0.5 w-full rounded-full bg-white/10 my-4' />
+          <div className='sm:block md:block lg:hidden xl:hidden h-0.5 w-full rounded-full bg-white/10' />
           
           <div>
             <h4 className='text-base text-white'>{ca('target')}</h4>
             <h1 className='text-3xl text-white font-bold'>8,000 ton</h1>
-            <div className='w-fit bg-white px-4 py-1 rounded-full mt-4'>
+            <div className='w-full bg-white px-4 py-1 rounded-full mt-4 drop-shadow-md'>
               <h6 className='text-sm text-zinc-800/70'><span className='text-green-500 font-bold'>+12.3%</span> {ca('notes')}</h6>
             </div>
           </div>
