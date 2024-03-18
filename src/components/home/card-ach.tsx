75ac70656f392555c49f9a7ba94d4e4532faafe8
Irvan Gerhana <weexdayend@gmail.com>
2024-03-19 02:39:14 +0700

    fixing layout ui and color card home

--- src/components/home/card-ach.tsx
+++ src/components/home/card-ach.tsx
@@ -6,12 +6,12 @@ const CardAch = () => {
   const ca = useTranslations('CardAch');
   
   return (
-    <div className='grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 lg:col-span-2 xl:grid-cols-2 xl:col-span-2 w-full h-full bg-gradient-to-br from-indigo-500 via-blue-400 to-purple-500 rounded-3xl shadow-lg shadow-black/5 gap-6 px-6 py-6'>
+    <div className='flex flex-col w-full h-full bg-gradient-to-br from-indigo-500 via-blue-400 to-purple-500 rounded-3xl shadow-lg shadow-black/5 gap-6 px-6 py-6'>
       <div className='flex flex-col w-full h-full justify-center'>
         <h4 className='text-base text-white'>Realisasi F5</h4>
         <h1 className='text-3xl text-white font-bold'>8.000 <span className='text-sm font-normal'>(Ton)</span></h1>
         <div className='w-full bg-white px-4 py-1 rounded-full mt-4 drop-shadow-md backdrop-filter backdrop-blur-lg bg-opacity-20'>
-          <h6 className='text-sm text-white'>dari total target 8.000 Ton</h6>
+          <h6 className='text-sm text-white'>dari target 8.000 Ton</h6>
         </div>
       </div>
 
@@ -21,7 +21,7 @@ const CardAch = () => {
         <h4 className='text-base text-white'>Realisasi F6</h4>
         <h1 className='text-3xl text-white font-bold'>8.000 <span className='text-sm font-normal'>(Ton)</span></h1>
         <div className='w-full bg-white px-4 py-1 rounded-full mt-4 drop-shadow-md backdrop-filter backdrop-blur-lg bg-opacity-20'>
-          <h6 className='text-sm text-white'>dari total target 8.000 Ton</h6>
+          <h6 className='text-sm text-white'>dari target 8.000 Ton</h6>
         </div>
       </div>
     </div>
--- src/components/home/card-overall.tsx
+++ src/components/home/card-overall.tsx
@@ -6,10 +6,12 @@ const CardOverall = () => {
   const cs = useTranslations('CardStock');
     
   return (
-    <div className='flex flex-col w-full h-full justify-center p-4'>
-      <h4 className='text-xl'>{cs('title')}</h4>
-      <h1 className='font-bold text-4xl'>10.000 <span className='text-base font-normal'>(Ton)</span></h1>
-      <h6 className='text-sm dark:text-white/60'>{cs('description')}</h6>
+    <div className='grid w-full h-full p-4'>
+      <div className='flex flex-col h-full w-full justify-center gap-2'>
+        <h4 className='text-xl'>{cs('title')}</h4>
+        <h1 className='font-bold text-4xl'>10.000 <span className='text-base font-normal'>(Ton)</span></h1>
+        <h6 className='text-sm dark:text-white/60'>{cs('description')}</h6>
+      </div>
     </div>
   )
 }
