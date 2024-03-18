180bf1a69ede61c155572fc691ff49de54ae93d1
Irvan Gerhana <weexdayend@gmail.com>
2024-03-18 18:23:04 +0700

    editing home

--- src/app/[locale]/(main)/home/page.tsx
+++ src/app/[locale]/(main)/home/page.tsx
@@ -9,11 +9,11 @@ const Page = () => {
   const ca = useTranslations('CardAch');
 
   return (
-    <div className='flex flex-col items-center pt-[15vh]'>
+    <div className='flex flex-col items-center pt-[15vh] py-16'>
       <div className='w-full grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-12 px-4'>
-        <div className='flex flex-col w-full rounded-xl shadow-lg shadow-black/5 p-4'>
+        <div className='flex flex-col w-full p-4'>
           <h1 className='text-xl'>{cs('title')}</h1>
-          <h1 className='font-bold text-5xl'>10,000 ton</h1>
+          <h1 className='font-bold text-4xl'>10,000 ton</h1>
         </div>
         <div className='flex flex-col w-full bg-zinc-800 rounded-xl shadow-lg shadow-black/5 dark:bg-white p-4'>
           <h4 className='text-lg text-white/70 dark:text-gray-600'>{ca('realization')}</h4>
@@ -26,7 +26,7 @@ const Page = () => {
           <h1 className='text-3xl text-white font-bold dark:text-gray-800'>8,000 ton</h1>
           <h6 className='text-sm text-white/70 dark:text-gray-700'><span className='text-green-500 font-bold'>12.3%</span> {ca('notes')}</h6>
         </div>
-        <div className='flex flex-col w-full rounded-xl shadow-lg shadow-black/5 p-4'>
+        <div className='flex flex-col w-full p-4'>
           <h1 className='text-2xl'>Distribution</h1>
           <h4 className='text-sm dark:text-white/60'>Stream of distribution fertilizer</h4>
           <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-6 pt-6'>
