97b524ddd727ef8c0032e5664b8ab28d23e557c6
Irvan Gerhana <weexdayend@gmail.com>
2024-03-19 02:46:37 +0700

    change gradient direction

--- src/app/[locale]/(main)/home/page.tsx
+++ src/app/[locale]/(main)/home/page.tsx
@@ -11,17 +11,17 @@ const Page = () => {
       <div className='w-full grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6 px-4'>
         <CardOverall />
         <div className='flex flex-col gap-4 w-full h-full'>
-          <div className='bg-gradient-to-r from-indigo-500 via-blue-400 to-blue-500 rounded-2xl xl:rounded-3xl h-full flex flex-row items-center justify-between px-5 py-4'>
+          <div className='bg-gradient-to-br from-indigo-500 via-blue-400 to-blue-500 rounded-2xl xl:rounded-3xl h-full flex flex-row items-center justify-between px-5 py-4'>
             <h1 className='text-white'>Distributor</h1>
             <h1 className='text-xl font-bold text-white'>17</h1>
           </div>
 
-          <div className='bg-gradient-to-r from-blue-500 via-blue-400 to-purple-500 rounded-2xl xl:rounded-3xl h-full flex flex-row items-center justify-between px-5 py-4'>
+          <div className='bg-gradient-to-br from-blue-500 via-blue-400 to-purple-500 rounded-2xl xl:rounded-3xl h-full flex flex-row items-center justify-between px-5 py-4'>
             <h1 className='text-white'>Kios</h1>
             <h1 className='text-xl font-bold text-white'>901</h1>
           </div>
 
-          <div className='bg-gradient-to-r from-purple-500 via-blue-400 to-indigo-500 rounded-2xl xl:rounded-3xl h-full flex flex-row items-center justify-between px-5 py-4'>
+          <div className='bg-gradient-to-br from-purple-500 via-blue-400 to-indigo-500 rounded-2xl xl:rounded-3xl h-full flex flex-row items-center justify-between px-5 py-4'>
             <h1 className='text-white'>Gudang Lini III</h1>
             <h1 className='text-xl font-bold text-white'>5</h1>
           </div>
