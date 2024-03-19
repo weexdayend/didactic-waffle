27e251f4d73600bf84495d4624c96f2b062f4f10
Irvan Gerhana <weexdayend@gmail.com>
2024-03-20 02:06:26 +0700

    impement reporting from excel

--- src/components/home/card-ach.tsx
+++ src/components/home/card-ach.tsx
@@ -8,22 +8,44 @@ const CardAch = () => {
   return (
     <div className='flex flex-col w-full h-full bg-gradient-to-br from-indigo-500 via-blue-400 to-purple-500 rounded-3xl shadow-lg shadow-black/5 gap-6 px-6 py-6'>
       <div className='flex flex-col w-full h-full justify-center'>
-        <h4 className='text-base text-white'>Realisasi F5</h4>
-        <h1 className='text-3xl text-white font-bold'>8.000 <span className='text-sm font-normal'>(Ton)</span></h1>
-        <div className='w-full bg-white px-4 py-1 rounded-full mt-4 drop-shadow-md backdrop-filter backdrop-blur-lg bg-opacity-20'>
-          <h6 className='text-sm text-white'>dari target 8.000 Ton</h6>
+        <h4 className='text-xl text-white'>Realisasi F5</h4>
+        <div className='grid grid-cols-3 gap-4'>
+          <div className='py-4 border-b-2 border-white'>
+            <h6 className='text-lg font-bold text-white'>2.000</h6>
+            <h6 className='text-sm text-white'>UREA</h6>
+          </div>
+          <div className='py-4 border-b-2 border-white'>
+            <h6 className='text-lg font-bold text-white'>2.000</h6>
+            <h6 className='text-sm text-white'>ORGANIK</h6>
+          </div>
+          <div className='py-4 border-b-2 border-white'>
+            <h6 className='text-lg font-bold text-white'>1.000</h6>
+            <h6 className='text-sm text-white'>NPK</h6>
+          </div>
         </div>
       </div>
 
       <div className='sm:block md:block lg:hidden xl:hidden h-0.5 w-full rounded-full bg-white/10' />
       
       <div className='flex flex-col w-full h-full justify-center'>
-        <h4 className='text-base text-white'>Realisasi F6</h4>
-        <h1 className='text-3xl text-white font-bold'>8.000 <span className='text-sm font-normal'>(Ton)</span></h1>
-        <div className='w-full bg-white px-4 py-1 rounded-full mt-4 drop-shadow-md backdrop-filter backdrop-blur-lg bg-opacity-20'>
-          <h6 className='text-sm text-white'>dari target 8.000 Ton</h6>
+        <h4 className='text-xl text-white'>Realisasi F6</h4>
+        <div className='grid grid-cols-3 gap-4'>
+          <div className='py-4 border-b-2 border-white'>
+            <h6 className='text-lg font-bold text-white'>2.000</h6>
+            <h6 className='text-sm text-white'>UREA</h6>
+          </div>
+          <div className='py-4 border-b-2 border-white'>
+            <h6 className='text-lg font-bold text-white'>2.000</h6>
+            <h6 className='text-sm text-white'>ORGANIK</h6>
+          </div>
+          <div className='py-4 border-b-2 border-white'>
+            <h6 className='text-lg font-bold text-white'>1.000</h6>
+            <h6 className='text-sm text-white'>NPK</h6>
+          </div>
         </div>
       </div>
+
+      <h6 className='text-sm text-white/70 mt-4'>Satuan dalam (Ton).</h6>
     </div>
   )
 }
--- src/components/home/card-overall.tsx
+++ src/components/home/card-overall.tsx
@@ -13,21 +13,21 @@ const CardOverall = () => {
           <h1 className='font-bold text-4xl text-white dark:text-zinc-900'>10.000 <span className='text-base font-normal'>(Ton)</span></h1>
           <h6 className='text-sm text-white dark:text-zinc-900 mt-4'>{cs('description')}</h6>
         </div>
-        <div className='grid grid-cols-3 gap-2 my-4'>
-          <div className='flex flex-col w-full h-full bg-zinc-900/30 px-2 py-4 rounded-xl'>
-            <h1 className='text-sm text-white dark:text-zinc-900'>4.000</h1>
+        <div className='grid grid-cols-3 gap-4 items-center justify-between my-4'>
+          <div className='flex flex-col'>
+            <h1 className='text-lg font-bold text-white dark:text-zinc-900'>4.000</h1>
             <h1 className='text-sm text-white dark:text-zinc-900'>UREA</h1>
           </div>
-          <div className='w-full h-full bg-zinc-900/30 px-2 py-4 rounded-xl'>
-            <h1 className='text-sm text-white dark:text-zinc-900'>4.000</h1>
+          <div className='w-full h-full'>
+            <h1 className='text-lg font-bold text-white dark:text-zinc-900'>4.000</h1>
             <h1 className='text-sm text-white dark:text-zinc-900'>ORGANIK</h1>
           </div>
-          <div className='w-full h-full bg-zinc-900/30 px-2 py-4 rounded-xl'>
-            <h1 className='text-sm text-white dark:text-zinc-900'>2.000</h1>
+          <div className='w-full h-full'>
+            <h1 className='text-lg font-bold text-white dark:text-zinc-900'>2.000</h1>
             <h1 className='text-sm text-white dark:text-zinc-900'>NPK</h1>
           </div>
         </div>
-        <h6 className='text-sm text-white dark:text-zinc-900 mt-4'>Satuan dalam (Ton).</h6>
+        <h6 className='text-sm text-white/70 dark:text-zinc-900/70 mt-4'>Satuan dalam (Ton).</h6>
       </div>
     </div>
   )
