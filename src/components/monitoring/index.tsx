9133ecd23ac7c6e220a128a976d4d5282d280295
Irvan Gerhana <weexdayend@gmail.com>
2024-04-16 11:46:51 +0700

    fixing monitoring

--- src/components/monitoring/index.tsx
+++ src/components/monitoring/index.tsx
@@ -20,7 +20,7 @@ import { Button } from '../ui/button';
 
 const Monitoring = () => {
   return (
-    <Drawer>
+    <Drawer preventScrollRestoration={false}>
       <DrawerTrigger>
         <div
             className='relative h-10 w-10 border border-red-500 bg-background hover:bg-accent hover:text-accent-foreground inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50'
@@ -36,42 +36,42 @@ const Monitoring = () => {
           <DrawerTitle>Monitoring Aplikasi</DrawerTitle>
           <DrawerDescription>Daftar status aplikasi pupuk indonesia.</DrawerDescription>
         </DrawerHeader>
-        <div className='w-full h-fit grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 md:col-span-4 lg:grid-cols-4 lg:col-span-4 xl:grid-cols-4 xl:col-span-4 gap-6 px-4 pb-6'>
-          <div className='border-2 border-green-500 rounded-3xl xl:rounded-3xl h-full flex flex-col sm:flex-row md:flex-row lg:flex-row xl:flex-row items-center justify-between px-6 py-6 gap-2'>
+        <div className='w-full h-fit grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 md:col-span-4 lg:grid-cols-4 lg:col-span-4 xl:grid-cols-4 xl:col-span-4 gap-2 px-4 pb-6'>
+          <div className='border-2 border-green-500 rounded-xl xl:rounded-xl h-fit flex flex-col sm:flex-row md:flex-row lg:flex-row xl:flex-row items-center justify-between px-3 py-3 gap-2'>
             <div className='flex flex-col gap-6'>
               <h1 className='text-lg text-white'>WCM</h1>
             </div>
-            <div className='flex flex-row w-full justify-between items-center px-4 py-2 rounded-full border border-green-500'>
+            <div className='flex flex-row w-full justify-between items-center px-4 py-2 rounded-xl border border-green-500'>
               <h1 className='text-xs'>ON</h1>
               <CheckCircle2Icon className='w-4 h-4 text-green-500' />
             </div>
           </div>
 
-          <div className='border-2 border-red-500 rounded-3xl xl:rounded-3xl h-full flex flex-col sm:flex-row md:flex-row lg:flex-row xl:flex-row items-center justify-between px-6 py-6 gap-2'>
+          <div className='border-2 border-red-500 rounded-xl xl:rounded-xl h-fit flex flex-col sm:flex-row md:flex-row lg:flex-row xl:flex-row items-center justify-between px-3 py-3 gap-2'>
             <div className='flex flex-col'>
               <h1 className='text-lg text-white'>APG</h1>
             </div>
-            <div className='flex flex-row w-full justify-between items-center px-4 py-2 rounded-full border border-red-500'>
+            <div className='flex flex-row w-full justify-between items-center px-4 py-2 rounded-xl border border-red-500'>
               <h1 className='text-xs'>OFF</h1>
               <Link2OffIcon className='w-4 h-4 text-red-500' />
             </div>
           </div>
 
-          <div className='border-2 border-green-500 rounded-3xl xl:rounded-3xl h-full flex flex-col sm:flex-row md:flex-row lg:flex-row xl:flex-row items-center justify-between px-6 py-6 gap-2'>
+          <div className='border-2 border-green-500 rounded-xl xl:rounded-xl h-fit flex flex-col sm:flex-row md:flex-row lg:flex-row xl:flex-row items-center justify-between px-3 py-3 gap-2'>
             <div className='flex flex-col'>
               <h1 className='text-lg text-white'>Rekan</h1>
             </div>
-            <div className='flex flex-row w-full justify-between items-center px-4 py-2 rounded-full border border-green-500'>
+            <div className='flex flex-row w-full justify-between items-center px-4 py-2 rounded-xl border border-green-500'>
               <h1 className='text-xs'>ON</h1>
               <CheckCircle2Icon className='w-4 h-4 text-green-500' />
             </div>
           </div>
 
-          <div className='border-2 border-green-500 rounded-3xl xl:rounded-3xl h-full flex flex-col sm:flex-row md:flex-row lg:flex-row xl:flex-row items-center justify-between px-6 py-6 gap-2'>
+          <div className='border-2 border-green-500 rounded-xl xl:rounded-xl h-fit flex flex-col sm:flex-row md:flex-row lg:flex-row xl:flex-row items-center justify-between px-3 py-3 gap-2'>
             <div className='flex flex-col'>
               <h1 className='text-lg text-white'>Markisa</h1>
             </div>
-            <div className='flex flex-row w-full justify-between items-center px-4 py-2 rounded-full border border-green-500'>
+            <div className='flex flex-row w-full justify-between items-center px-4 py-2 rounded-xl border border-green-500'>
               <h1 className='text-xs'>ON</h1>
               <CheckCircle2Icon className='w-4 h-4 text-green-500' />
             </div>
